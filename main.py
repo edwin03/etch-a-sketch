f0aa68bcf969352705ff9c9f4a2c4e83ff42fb8e
Edwin Rubio <edwin@Edwins-Air.tailad7ce3.ts.net lan>
2026-07-15 06:37:14 -0500

    Added key listeners.

--- main.py
+++ main.py
@@ -8,6 +8,27 @@ screen.listen()
 def mov_fd():
     tim.fd(10)
 
-screen.onkey(mov_fd, "space")
+def mov_lf():
+    tim.seth(180)
+    tim.fd(10)
+
+def mov_rh():
+    tim.seth(0)
+    tim.fd(10)
+
+def mov_dw():
+    tim.seth(270)
+    tim.fd(10)
+
+def clear():
+    screen.clearscreen()
+    tim.home()
+
+screen.onkey(mov_fd, "w")
+screen.onkey(mov_dw, "s")
+screen.onkey(mov_lf, "a")
+screen.onkey(mov_rh, "d")
+screen.onkey(clear, "c")
+
 
 screen.exitonclick()
